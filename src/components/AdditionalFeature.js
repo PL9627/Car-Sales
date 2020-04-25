2d7a58483be953b3f9b07d220f393d7f854479eb
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-24 17:35:42 -0700

    imported addPrice and added an onClick event for AdditionalFeature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {addPrice} from '../actions/priceChange';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => {
+        props.addPrice(props.feature)
+      }}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
