86430896cc037fa01cdb987284e3a20045ce0d58
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-24 17:40:53 -0700

    finished mapStateToProps in AdditionalFeatures

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {addPrice} from '../actions/priceChange';
+import React from "react";
+import { addPrice } from "../actions/priceChange";
 
-const AdditionalFeature = props => {
+const AdditionalFeature = (props) => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button" onClick={() => {
-        props.addPrice(props.feature)
-      }}>Add</button>
+      <button
+        className="button"
+        onClick={() => {
+          props.addPrice(props.feature);
+        }}
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
